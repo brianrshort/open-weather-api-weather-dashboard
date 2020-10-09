95013f541a8311536835e2321b544cfdf2c67910
Brian Short <brian.ralph.short@gmail.com>
2020-10-09 01:32:09 -0400

    Got dynamic content container (single day) to populate correctly; got UV index element to populate correctly with color code

---
+++ script.js
@@ -0,0 +1,66 @@
+$( document ).ready(function() {
+    var appID = "330bdacad723effeefd38103fc953d4e";
+
+    $(".query_btn").click(function(){
+
+        var query_param = $(this).prev().val();
+
+        var longitude;
+        var latitude;
+
+        var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
+        
+
+       
+
+        $.getJSON(weather,function(json){
+            $("#city").html(json.name);
+            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+            $("#main_weather").html(`Weather: ${json.weather[0].main}`);
+            $("#temperature").html(`Temperature: ${json.main.temp} degrees`);
+            $("#humidity").html(`Humidity: ${json.main.humidity}%`);
+            longitude = json.coord.lon;
+            console.log(longitude);
+            latitude = json.coord.lat;
+            console.log(latitude);              
+           
+            var uvi = `http://api.openweathermap.org/data/2.5/uvi/forecast?lat=${latitude}&lon=${longitude}&appid=${appID}`;
+
+            $.getJSON(uvi, function(json) {
+                $("#uvi").html(`UVI: <span id="uvi-color">${json[0].value}</span>`);
+                console.log(json[0].value);
+                if (json[0].value <= 2) {
+                    $("#uvi-color").addClass("green");
+                } else if (json[0].value <= 5) {
+                    $("#uvi-color").addClass("yellow");
+                } else if (json[0].value <= 7) {
+                    $("#uvi-color").addClass("orange");
+                } else if (json[0].value <= 10) {
+                    $("#uvi-color").addClass("red");
+                }
+            })
+        
+        });
+
+    })
+
+
+
+
+    // Optional Code for temperature conversion
+    var fahrenheit = true;
+
+    $("#convertToCelsius").click(function() {
+        if (fahrenheit) {
+            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
+        }
+        fahrenheit = false;
+    });
+
+    $("#convertToFahrenheit").click(function() {
+        if (fahrenheit == false) {
+            $("#temperature").text((($("#temperature").text() * (9/5)) + 32));
+        }
+        fahrenheit = true;
+    });
+});
